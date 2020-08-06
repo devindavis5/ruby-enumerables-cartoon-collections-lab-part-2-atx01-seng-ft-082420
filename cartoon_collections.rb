8e95f61cc63e466e3bdf455bc9a6bb5a4ae53f55
Devin Davis <devinrdavis555@gmail.com>
2020-08-06 16:58:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   
-  array.map{|element| element **}
+  array.map{|element| element * element }
   
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
